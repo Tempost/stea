9c672fb8996677f0d4d9c26b32f2d40bf59885f1
Tempost <codyldiamond@gmail.com>
2022-06-02 22:11:12 -0500

    init of api, fleshed out trpc router. need to rename methods and write the code

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -2,6 +2,35 @@ import * as trpc from "@trpc/server";
 import { z } from "zod";
 import { prisma } from "@/backend/utils/prisma";
 
-export const appRouter = trpc.router();
+const createRouter = () => {
+  return trpc.router<Context>();
+}
 
+
+  const member = createRouter()
+  .query("get-members")
+  .mutation("add-member");
+
+  const horse = createRouter()
+  .query("get-horses")
+  .mutation("add-horse");
+
+  const ranking = createRouter()
+  .query("get-rankings")
+  .mutation("update-ranking");
+
+  const family = createRouter()
+  .query("get-family")
+  .mutation("update-family");
+
+  const show = createRouter()
+  .query("get-show")
+  .mutation("add-show");
+
+export const appRouter = createRouter()
+  .merge('member.', member)
+  .merge('horse.', horse)
+  .merge('ranking.', ranking)
+  .merge('family.', family)
+  .merge('shows', show)
 export type AppRouter = typeof appRouter;
