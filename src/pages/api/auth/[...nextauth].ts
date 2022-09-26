374249ee9827845f98c1c7bd1d5ef2240fac6d7e
Cody <codyldiamond@gmail.com>
2022-09-25 19:46:35 -0500

    google auth for dashboard route completed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,17 +3,30 @@ import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   callbacks: {
-    session({ session, token, user }) {
+    session({ session, token }) {
+      session.access_token = token.access_token;
+      console.log('this is the current session.', session);
       return session;
     },
-    signIn({ user, account, profile, email, credentials }) {
-      return true;
-    },
+    jwt({ token, account }) {
+      console.log(account)
+      if (account) {
+        token.access_token = account.access_token;
+      }
+      return token;
+    }
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      },
     }),
   ],
 });
