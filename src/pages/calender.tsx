5e90ab4a2cd6d8c3411e319bbed2e892b8c1ff35
Cody <codyldiamond@gmail.com>
2022-10-07 20:29:52 -0500

    Coming soon calender, contact us page(need contact form eventually)

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -1,7 +1,15 @@
 import { ReactElement } from 'react';
 import { PublicLayout } from '@/components/layout';
 
-function SteaCalender() {}
+function SteaCalender() {
+  return (
+    <section className='flex flex-col justify-center items-center h-full'>
+      <h1 className='text-lg md:text-2xl font-bold text-neutral mx-auto w-fit'>
+        Coming soon...
+      </h1>
+    </section>
+  );
+}
 
 SteaCalender.getLayout = (page: ReactElement) => {
   return <PublicLayout>{page}</PublicLayout>;
--- src/pages/contactus.tsx
+++ src/pages/contactus.tsx
@@ -1,6 +1,76 @@
-import { ReactElement } from 'react';
+import { PropsWithChildren, ReactElement } from 'react';
 import { PublicLayout } from '@/components/layout';
-function ContactUs() {}
+
+function ContactCard({ children }: PropsWithChildren) {
+  return <p className='rounded-md shadow-2xl w-fit p-5'>{children}</p>;
+}
+function ContactUs() {
+  return (
+    <section className='flex flex-col justify-center items-center h-full'>
+      <h1 className='text-lg md:text-2xl font-bold text-neutral mx-auto w-fit'>
+        Boardmember Contact Information
+      </h1>
+      <div className='container mx-auto flex flex-row flex-wrap justify-center gap-10 m-10'>
+        <ContactCard>
+          New Horizon Stables
+          <br />
+          Lynette Diamond
+          <br />
+          25498 Macedonia Road
+          <br />
+          Hockley, TX 77447
+          <br />
+          (832) 766-8400
+          <br />
+          newhorizonstables@sbcglobal.net
+          <br />
+        </ContactCard>
+        <ContactCard>
+          Pine Hill
+          <br />
+          Ruth Swain
+          <br />
+          1720 Hwy. 159 East
+          <br />
+          Bellville, TX 77418
+          <br />
+          (979) 865-5591
+          <br />
+          pinehilltexas@sbcglobal.net
+          <br />
+        </ContactCard>
+        <ContactCard>
+          Meadow Creek Park
+          <br />
+          Kaitlyn Arnold
+          <br />
+          1342 State Hwy 14
+          <br />
+          Kosse, TX 76653
+          <br />
+          (512) 227-0678
+          <br />
+          meadowcreekparktexas@gmail.com
+          <br />
+        </ContactCard>
+        <ContactCard>
+          Snowdonia Sporthorse Complex
+          <br />
+          Jayne Lloyd
+          <br />
+          6714 Neiman Road
+          <br />
+          Brookshire, TX 77423
+          <br />
+          (281) 460-9107
+          <br />
+          snowdoniasporthc@yahoo.com
+          <br />
+        </ContactCard>
+      </div>
+    </section>
+  );
+}
 ContactUs.getLayout = (page: ReactElement) => {
   return <PublicLayout>{page}</PublicLayout>;
 };
