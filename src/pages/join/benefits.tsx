f79cc45234145b9613f83e77b97f0ac50a932f4f
fluffybannana <michael.owen.mro@gmail.com>
2022-12-07 18:35:09 -0600

    mobile friendly

--- src/pages/join/benefits.tsx
+++ src/pages/join/benefits.tsx
@@ -2,40 +2,40 @@ import { PublicLayout } from '@/components/layout';
 function SteaBeneifits() {
   return (
     <div className='text-center'>
-      <div className='hero min-h-screen bg-[url(/train_jump_1440.webp)] bg-fixed'>
-        <div className='hero-overlay bg-opacity-60'></div>
+      <div className='hero min-h-screen bg-[url(/train_jump_1440.webp)] bg-fixed '>
+        <div className='hero-overlay bg-opacity-50 '></div>
       
-        <div className=' rounded-2xl hero-content text-center text-neutral-content h-24 w-3/6 flex-col'>
-          <h1 className=' text-Black-100 text-center text-7xl'>Rewards:</h1>
+        <div className=' rounded-2xl hero-content text-center text-neutral-content h-24  flex-col  w-full'>
+          <h1 className=' text-Black-100 text-center lg:text-7xl sm:text-5xl'>Rewards:</h1>
           <h1 className=' text-Black-100 text-center text-9xl'> </h1>
-          <h1 className='text-amber-400 text-center text-5xl font-bold font-mono'>
+          <h1 className='text-amber-500 text-center lg:text-5xl font-bold font-mono sm:text-3xl md:text-4xl'>
             Gold Level: $200 (50 hours)
           </h1>
-          <h1 className=' text-slate-300  text-center text-5xl font-bold font-mono'>
+          <h1 className=' text-slate-400  text-center lg:text-5xl font-bold font-mono sm:text-3xl md:text-4xl'>
             Silver Level:$100 (25 hours)
           </h1>
-          <h1 className='text-amber-700 text-center text-5xl font-bold font-mono'>
+          <h1 className='text-amber-800 text-center lg:text-5xl font-bold font-mono sm:text-3xl md:text-4xl'>
             Bronze Level:$40 (10 hours)
           </h1>
-          <h1 className=' text-Black-100 text-center text-9xl'>   </h1>
-          <h1 className=' text-Black-100 text-center text-9xl'>   </h1>
-          <h1 className=' text-Black-100 text-center text-9xl'>   </h1>
-          <h1 className='text-red-500 text-center text-5xl font-bold'>
+          <h1 className=' text-Black-100 text-center lg:text-9xl '>   </h1>
+          <h1 className=' text-Black-100 text-center lg:text-9xl '>   </h1>
+          <h1 className=' text-Black-100 text-center lg:text-9xl '>   </h1>
+          <h1 className='text-red-500 text-center md:text-5xl font-bold sm:text-3xl'>
             IMPORTANT
           </h1>
-          <h1 className='text-black-200 text-center text-2xl'>
+          <h1 className='text-black-200 text-center lg:text-2xl md:text-xl sm:text-lg'>
             1: Must be a current Stea member to cache in hours
           </h1>
-          <h1 className='text-black-200 text-center text-2xl'>
+          <h1 className='text-black-200 text-center lg:text-2xl md:text-xl sm:text-lg'>
             2: May only earn 5 hours per day, per event
           </h1>
-          <h1 className='text-black-200 text-center text-2xl'>
+          <h1 className='text-black-200 text-center lg:text-2xl md:text-xl sm:text-lg'>
             3: may only cache hours once per year, per member
           </h1>
-          <h1 className='text-black-200 text-center text-2xl'>
+          <h1 className='text-black-200 text-center lg:text-2xl md:text-xl sm:text-lg'>
             4: PHYSICAL volunteer form must be signed by the show Secretary
           </h1>
-          <h1 className='text-black-200 text-center text-2xl'>
+          <h1 className='text-black-200 text-center lg:text-2xl md:text-xl sm:text-lg'>
             5: submit your form online for approval
           </h1>
         </div>
@@ -45,7 +45,7 @@ function SteaBeneifits() {
         Submit your volunteer form Here
       </h1>
       <form
-        action='uploadPlaceholder'
+        action='placeholder'
         encType='multipart/form-data'
       >
         <input
