efd758d338616e27f3a0ee80d123db5fbc7a1b29
Cody <codyldiamond@gmail.com>
2022-12-07 19:05:16 -0600

    TRPC errors when serving on vercel, this might be the fix?

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -6,19 +6,20 @@ import type {
   inferRouterOutputs,
   inferRouterDef,
 } from '@trpc/server';
-
 import { AppRouter } from '@/server/router/_app';
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
 
 export const transformer = superjson;
 export const trpc = createTRPCNext<AppRouter>({
   config() {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : 'http://localhost:3000';
 
     return {
       transformer,
-      url,
       queryClientConfig: {
         defaultOptions: {
           queries: {
@@ -34,7 +35,7 @@ export const trpc = createTRPCNext<AppRouter>({
             (opts.direction === 'down' && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${url}/api/trpc`,
+          url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
     };
