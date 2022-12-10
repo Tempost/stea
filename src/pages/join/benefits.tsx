4e91e05e8c0354d5a820c5bef3076f32d435ae55
fluffybannana <michael.owen.mro@gmail.com>
2022-12-09 18:07:50 -0600

    added background - teemp quotation marks for links

--- src/pages/join/benefits.tsx
+++ src/pages/join/benefits.tsx
@@ -2,10 +2,10 @@ import { PublicLayout } from '@/components/layout';
 function SteaBeneifits() {
   return (
     <div className='text-center'>
-      <div className='hero min-h-screen bg-[url(/train_jump_1440.webp)] bg-fixed '>
-        <div className='hero-overlay bg-opacity-50 '></div>
+      <div className='hero min-h-screen bg-[url(/membersandhorses.jpg)] bg-fixed '>
+        <div className='hero-overlay bg-opacity-50'></div>
       
-        <div className=' rounded-2xl hero-content text-center text-neutral-content h-24  flex-col  w-full'>
+        <div className=' rounded-2xl hero-content text-center text-neutral-content flex-col  w-auto bg-slate-500 bg-opacity-50 h-auto'>
           <h1 className=' text-Black-100 text-center lg:text-7xl sm:text-5xl'>Rewards:</h1>
           <h1 className=' text-Black-100 text-center text-9xl'> </h1>
           <h1 className='text-amber-500 text-center lg:text-5xl font-bold font-mono sm:text-3xl md:text-4xl'>
@@ -18,8 +18,7 @@ function SteaBeneifits() {
             Bronze Level:$40 (10 hours)
           </h1>
           <h1 className=' text-Black-100 text-center lg:text-9xl '>   </h1>
-          <h1 className=' text-Black-100 text-center lg:text-9xl '>   </h1>
-          <h1 className=' text-Black-100 text-center lg:text-9xl '>   </h1>
+          
           <h1 className='text-red-500 text-center md:text-5xl font-bold sm:text-3xl'>
             IMPORTANT
           </h1>
@@ -33,10 +32,10 @@ function SteaBeneifits() {
             3: may only cache hours once per year, per member
           </h1>
           <h1 className='text-black-200 text-center lg:text-2xl md:text-xl sm:text-lg'>
-            4: PHYSICAL volunteer form must be signed by the show Secretary
+            4: PHYSICAL 'volunteer form' must be signed by the show Secretary
           </h1>
           <h1 className='text-black-200 text-center lg:text-2xl md:text-xl sm:text-lg'>
-            5: submit your form online for approval
+            5: submit your form online for approval 'Here'
           </h1>
         </div>
         <div className=' rounded-3xl  h-80 w-5/6 flex-col'></div>
