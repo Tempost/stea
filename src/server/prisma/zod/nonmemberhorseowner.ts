05b51d979654c44be082e61da2b841cb3b6a508b
Cody Diamond <cody_diamond@tecnics.com>
2023-01-06 09:33:19 -0600

    Prettier & Config

--- src/server/prisma/zod/nonmemberhorseowner.ts
+++ src/server/prisma/zod/nonmemberhorseowner.ts
@@ -1,20 +1,21 @@
-import * as z from "zod"
-import { PhoneType } from "@prisma/client"
-import { CompleteHorse, RelatedHorseModel } from "./index"
+import * as z from 'zod';
+import { PhoneType } from '@prisma/client';
+import { CompleteHorse, RelatedHorseModel } from './index';
 
 export const NonMemberHorseOwnerModel = z.object({
   createdAt: z.date().nullish(),
   updatedAt: z.date().nullish(),
-  firstName: z.string().trim().min(1, { message: "First Name is required" }),
-  lastName: z.string().trim().min(1, { message: "Last Name is required" }),
+  firstName: z.string().trim().min(1, { message: 'First Name is required' }),
+  lastName: z.string().trim().min(1, { message: 'Last Name is required' }),
   fullName: z.string().trim(),
-  email: z.string().trim().email({ message: "Invalid email address" }),
-  phone: z.string().trim().min(1, { message: "Phone number is required" }),
+  email: z.string().trim().email({ message: 'Invalid email address' }),
+  phone: z.string().trim().min(1, { message: 'Phone number is required' }),
   phoneType: z.nativeEnum(PhoneType),
-})
+});
 
-export interface CompleteNonMemberHorseOwner extends z.infer<typeof NonMemberHorseOwnerModel> {
-  horses: CompleteHorse[]
+export interface CompleteNonMemberHorseOwner
+  extends z.infer<typeof NonMemberHorseOwnerModel> {
+  horses: CompleteHorse[];
 }
 
 /**
@@ -22,9 +23,12 @@ export interface CompleteNonMemberHorseOwner extends z.infer<typeof NonMemberHor
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const RelatedNonMemberHorseOwnerModel: z.ZodSchema<CompleteNonMemberHorseOwner> = z.lazy(() => NonMemberHorseOwnerModel.extend({
-  /**
-   * List of Horses that are linked to a member
-   */
-  horses: RelatedHorseModel.array(),
-}))
+export const RelatedNonMemberHorseOwnerModel: z.ZodSchema<CompleteNonMemberHorseOwner> =
+  z.lazy(() =>
+    NonMemberHorseOwnerModel.extend({
+      /**
+       * List of Horses that are linked to a member
+       */
+      horses: RelatedHorseModel.array(),
+    })
+  );
