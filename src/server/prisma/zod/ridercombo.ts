e752faaeb17deb3850063fc7e4197e64b367ebfe
Cody <codyldiamond@gmail.com>
2023-01-24 15:50:16 -0600

    Updated packages

--- src/server/prisma/zod/ridercombo.ts
+++ src/server/prisma/zod/ridercombo.ts
@@ -1,17 +1,8 @@
-import * as z from 'zod';
-import {
-  CompleteMember,
-  RelatedMemberModel,
-  CompleteHorse,
-  RelatedHorseModel,
-  CompletePoints,
-  RelatedPointsModel,
-  CompleteShow,
-  RelatedShowModel,
-} from './index';
+import * as z from "zod"
+import { CompleteMember, RelatedMemberModel, CompleteHorse, RelatedHorseModel, CompletePoints, RelatedPointsModel, CompleteShow, RelatedShowModel } from "./index"
 
 export const RiderComboModel = z.object({
-  uid: z.string().cuid({ message: 'Invalid cuid' }),
+  uid: z.string().cuid({ message: "Invalid cuid" }),
   createdAt: z.date().nullish(),
   updatedAt: z.date().nullish(),
   division: z.string().trim(),
@@ -19,15 +10,15 @@ export const RiderComboModel = z.object({
   totalShows: z.number().int(),
   completedHT: z.boolean(),
   multiVenue: z.boolean(),
-  memberName: z.string().trim().min(1, { message: 'Member Name is required' }),
-  horseName: z.string().trim().min(1, { message: 'Horse Name is required' }),
-});
+  memberName: z.string().trim().min(1, { message: "Member Name is required" }),
+  horseName: z.string().trim().min(1, { message: "Horse Name is required" }),
+})
 
 export interface CompleteRiderCombo extends z.infer<typeof RiderComboModel> {
-  member: CompleteMember;
-  horse: CompleteHorse;
-  points: CompletePoints[];
-  shows: CompleteShow[];
+  member: CompleteMember
+  horse: CompleteHorse
+  points: CompletePoints[]
+  shows: CompleteShow[]
 }
 
 /**
@@ -35,12 +26,9 @@ export interface CompleteRiderCombo extends z.infer<typeof RiderComboModel> {
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const RelatedRiderComboModel: z.ZodSchema<CompleteRiderCombo> = z.lazy(
-  () =>
-    RiderComboModel.extend({
-      member: RelatedMemberModel,
-      horse: RelatedHorseModel,
-      points: RelatedPointsModel.array(),
-      shows: RelatedShowModel.array(),
-    })
-);
+export const RelatedRiderComboModel: z.ZodSchema<CompleteRiderCombo> = z.lazy(() => RiderComboModel.extend({
+  member: RelatedMemberModel,
+  horse: RelatedHorseModel,
+  points: RelatedPointsModel.array(),
+  shows: RelatedShowModel.array(),
+}))
