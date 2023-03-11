194d151fc0617afd1af94efbcf9925b4b57bf89d
Cody <codyldiamond@gmail.com>
2023-03-10 19:21:24 -0600

    Start of AddMember component via dashboard

--- src/components/forms/dashboard/NewMemberForm.tsx
+++ src/components/forms/dashboard/NewMemberForm.tsx
@@ -1,14 +1,18 @@
 import Input from '@/components/data-entry/Input';
+import Select from '@/components/data-entry/Select';
 import Modal from '@/components/styled-ui/Modal';
-import { MemberCreateInputSchema } from '@/server/prisma/zod-generated/inputTypeSchemas/MemberCreateInputSchema';
+import { PhoneTypeSchema } from '@/server/prisma/zod-generated/inputTypeSchemas/PhoneTypeSchema';
 import useZodForm from '@/utils/usezodform';
+import { MemberFormSchema } from '@/utils/zodschemas';
 import Form from '../Form';
+import states from '@/utils/states.json';
+import { capitalize } from '@/utils/helpers';
 
 function NewMemberForm() {
   const form = useZodForm({
     reValidateMode: 'onSubmit',
     shouldFocusError: true,
-    schema: MemberCreateInputSchema,
+    schema: MemberFormSchema.shape.member,
     defaultValues: {
       businessName: null,
     },
@@ -17,7 +21,7 @@ function NewMemberForm() {
   const { register } = form;
 
   // TODO: Better type after creating form
-  function onSubmit(formValues: any) { }
+  function onSubmit(formValues: any) {}
 
   return (
     <Modal
@@ -41,7 +45,83 @@ function NewMemberForm() {
       >
         <h3 className='text-lg font-bold'>Enter Member Information</h3>
         <div className='flex flex-row gap-5'>
-          <Input />
+          <Input
+            className='input-bordered input-primary input w-full md:input-sm'
+            type='text'
+            label='First Name'
+            {...register('firstName')}
+          />
+
+          <Input
+            className='input-bordered input-primary input w-full md:input-sm'
+            type='text'
+            label='Last Name'
+            {...register('lastName')}
+          />
+
+          <Input
+            className='input-bordered input-primary input w-full md:input-sm'
+            type='text'
+            placeholder='Address Line 1'
+            {...register('address')}
+          />
+
+          <Input
+            className='input-bordered input-primary input w-full md:input-sm'
+            type='text'
+            placeholder='City'
+            {...register('city')}
+          />
+
+          <Select
+            className='select-bordered select-primary select w-full md:select-sm'
+            {...register('state')}
+          >
+            {states.map(state => (
+              <option
+                key={state.value}
+                value={state.value}
+              >
+                {capitalize(state.label)}
+              </option>
+            ))}
+          </Select>
+
+          <Input
+            className='input-bordered input-primary input w-full md:input-sm'
+            type='numeric'
+            placeholder='Zip Code'
+            {...register('zip')}
+          />
+
+          <Select
+            label='Phone Type*'
+            className='select-bordered select-primary select md:select-sm'
+            {...register('phoneType')}
+          >
+            {Object.keys(PhoneTypeSchema.enum).map(type => (
+              <option
+                key={type}
+                value={type}
+              >
+                {type}
+              </option>
+            ))}
+          </Select>
+
+          <Input
+            className='input-bordered input-primary input w-full md:input-sm'
+            type='text'
+            label='Phone Number'
+            {...register('phone')}
+          />
+
+          <Input
+            className='input-bordered input-primary input w-full md:input-sm'
+            type='text'
+            label='Email'
+            {...register('email')}
+          />
         </div>
       </Form>
     </Modal>
