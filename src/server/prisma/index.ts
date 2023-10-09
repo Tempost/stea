d20f6ab59375dbd2ec8f5fe262a7e7901be684b4
Cody Diamond <cody_diamond@tecnics.com>
2023-10-09 10:16:56 -0500

    Updated prisma 5.4.1, enabled prisma accelerate

--- src/server/prisma/index.ts
+++ src/server/prisma/index.ts
@@ -1,18 +1,20 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prismaGlobal = global as typeof global & {
   prisma?: PrismaClient;
 };
 
 export const prisma =
-  prismaGlobal.prisma ||
+  prismaGlobal.prisma ??
   new PrismaClient({
     log:
       process.env.NODE_ENV === 'development'
         ? ['info', 'error', 'warn']
         : ['error'],
-  });
+  }).$extends(withAccelerate());
 
+// @ts-ignore TODO: Fix this type issue
 if (process.env.NODE_ENV !== 'production') prismaGlobal.prisma = prisma;
 
 export type MyPrismaClient = typeof prisma;
