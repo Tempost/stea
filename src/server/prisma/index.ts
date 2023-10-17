60783758b80baa3956c54e65b8427c3ce0c5db81
Cody Diamond <cody_diamond@tecnics.com>
2023-10-17 09:35:25 -0500

    Correct typing for prisma client with extends

--- src/server/prisma/index.ts
+++ src/server/prisma/index.ts
@@ -1,18 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-const prismaGlobal = global as typeof global & {
-  prisma?: PrismaClient;
-};
-
-export const prisma =
-  prismaGlobal.prisma ??
-  new PrismaClient({
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     log:
       process.env.NODE_ENV === 'development'
         ? ['info', 'error', 'warn']
         : ['error'],
   }).$extends(withAccelerate());
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const prismaGlobal = global as typeof global & {
+  prisma?: PrismaClientSingleton;
+};
+
+export const prisma = prismaGlobal.prisma ?? prismaClientSingleton();
 
 // @ts-ignore TODO: Fix this type issue
 if (process.env.NODE_ENV !== 'production') prismaGlobal.prisma = prisma;
