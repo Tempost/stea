9bb305375657edeebd1ae68d8b9b54a251270cc6
cody <codyldiamond@gmail.com>
2023-11-12 16:09:20 -0600

    Logic for hiding year select component

--- src/components/forms/RegistrationYearSelect.tsx
+++ src/components/forms/RegistrationYearSelect.tsx
@@ -10,9 +10,16 @@ interface Props
   heading: string;
   watchFieldName: string;
   register: UseFormRegisterReturn;
+  // TODO: Find proper type for this prop
   // eslint-disable-next-line
   control: any;
 }
+
+const today = new Date(2023, 9, 2);
+const curr = new Date(today.getFullYear(), 10, 30);
+const next = new Date(today.getFullYear() + 1, 10, 30);
+const from = new Date(today.getFullYear(), 8, 1);
+const to = new Date(today.getFullYear(), 11, 1);
 
 function RegistrationYearSelect({
   heading,
@@ -22,29 +29,33 @@ function RegistrationYearSelect({
   control,
 }: Props) {
   const annual = useWatch({ name: watchFieldName, control });
-  const currentDate = new Date(Date.now());
-  const currYear = new Date(currentDate.getFullYear(), 10, 30);
-  const nextYear = new Date(currYear.getFullYear() + 1, 10, 30);
 
-  return (
-    <section className={`${annual === 'Annual' ? '' : 'hidden'}`}>
-      <h3>{heading} (New year starts Novemeber 30th)</h3>
-      <Radio
-        label={`Current Year (${currYear.getFullYear()})`}
-        value={currYear.toString()}
-        className='radio-primary radio align-middle md:radio-sm'
-        onClick={onClick}
-        {...register}
-      />
-      <Radio
-        label={`Coming Year (${nextYear.getFullYear()})`}
-        value={nextYear.toString()}
-        className='radio-primary radio align-middle md:radio-sm'
-        onClick={onClick}
-        {...register}
-      />
-    </section>
-  );
+  const showComponent = today >= from && today <= to;
+  console.log(today, from, to, showComponent);
+
+  if (annual === 'Annual' && showComponent) {
+    return (
+      <section>
+        <h3>{heading} (New year starts Novemeber 30th)</h3>
+        <Radio
+          label={`Current Year (${curr.getFullYear()})`}
+          value={curr.toString()}
+          className='radio-primary radio align-middle md:radio-sm'
+          onClick={onClick}
+          {...register}
+        />
+        <Radio
+          label={`Coming Year (${next.getFullYear()})`}
+          value={next.toString()}
+          className='radio-primary radio align-middle md:radio-sm'
+          onClick={onClick}
+          {...register}
+        />
+      </section>
+    );
+  } else {
+    return <></>;
+  }
 }
 
 export default RegistrationYearSelect;
