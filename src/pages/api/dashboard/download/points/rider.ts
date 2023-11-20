5b0fef10f2493c9dc566817b36ef2cfe20fa1c74
cody <codyldiamond@gmail.com>
2023-11-20 07:29:22 -0600

    node-csv package install and setup streaming downloading of rider points

--- src/pages/api/dashboard/download/points/rider.ts
+++ src/pages/api/dashboard/download/points/rider.ts
@@ -1,7 +1,10 @@
-import { prisma } from '@/server/prisma';
+import { prisma, MyPrismaClient } from '@/server/prisma';
+import type { Prisma } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getToken } from 'next-auth/jwt';
 import { z, ZodError } from 'zod';
+import { stringify } from 'csv';
+import fs from 'fs';
 
 export default async function handler(
   req: NextApiRequest,
@@ -24,20 +27,34 @@ export default async function handler(
     const params = queryParams.parse(req.query);
 
     // TODO: The rider field will be the UID of the selected rider combo from the UI
-    const points = await prisma.points.findMany({
+    const points = await prisma.riderCombo.findMany({
       where: {
-        riderUid: params.rider,
+        uid: params.rider,
       },
     });
 
-    console.log(points);
+    const readSteam = fs.createReadStream('csv-file.csv');
 
-    return res.status(200).json(points);
+    const test = stringify({
+      header: true,
+      columns: ['memberName', 'totalPoints', 'totalShows', 'horseName'],
+    });
+
+    await new Promise(function (resolve) {
+      console.log('Writing data to stream');
+      points.forEach(row => test.write(row));
+
+      res.setHeader('Content-Type', 'application/csv');
+
+      // TODO: Update file name with the riders name
+      res.setHeader('Content-Disposition', `attachment; filename=what.csv`);
+      test.pipe(res).on('close', resolve);
+      readSteam.pipe(res);
+    });
   } catch (error) {
     console.log(error);
     return res.status(400).json({ message: '' });
   }
-
   res.end();
 }
 
