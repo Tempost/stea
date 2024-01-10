c79c5d4e1ea7827da65acf2e37c85f02a12a717d
Cody Diamond <cody_diamond@tecnics.com>
2024-01-10 16:09:04 -0600

    ?

--- src/server/router/utils.ts
+++ src/server/router/utils.ts
@@ -53,3 +53,15 @@ export async function checkExistingHorses(
     return matches;
   }
 }
+
+export function setMembershipYear() {
+  const currDate = new Date();
+  const endDate = new Date(currDate.getFullYear(), 10, 30);
+
+  // If the current month is decemeber
+  if (currDate.getMonth() == 11) {
+    endDate.setFullYear(endDate.getFullYear() + 1);
+  }
+
+  return endDate;
+}
